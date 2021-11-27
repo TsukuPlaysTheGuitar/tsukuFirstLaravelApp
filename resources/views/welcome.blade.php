8d2b90b62a16b6fb8f43fc39daba4e1cb9a13c74
Tsukuru Suzuki <suzukitsukuru@Tsukuru-Suzuki-MacBook-Pro.local>
2021-11-27 21:27:06 +0900

    edited_welcomeHtml_and_added_homeHtml

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset='utf-8'>
+        <title>トップ画面</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>トップ画面</h1>
+        </div>
+    </body>
+</html>
